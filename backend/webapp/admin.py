df3d4c3c2829083a7c0745fb4afae20e82784930
Dan <39170265+chillenberger@users.noreply.github.com>
2022-05-19 15:55:42 -0600

    remove backend

--- backend/webapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Account_model, Shipping_model, User_model
-
-# Registed models.
-admin.site.register([Account_model, Shipping_model, User_model])
